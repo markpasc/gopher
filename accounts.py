95eb72aba73d54e22e2bec3717f3f7dc8adc499e
Mark Paschal <markpasc@markpasc.org>
2009-05-13 14:50:27 -0700

    Accounts can be configured

--- accounts.py
+++ accounts.py
@@ -1,3 +1,8 @@
+import httplib2
+from oauth.oauth import OAuthConsumer
+
+from oauthclient import NetflixHttp
+import settings
 
 
 class Account(object):
@@ -7,11 +12,41 @@ class Account(object):
 
 
 class Netflix(Account):
-    pass
+
+    def configure(self):
+        if hasattr(self, 'access_token'):
+            return
+
+        h = NetflixHttp()
+        h.consumer = OAuthConsumer(settings.NETFLIX_KEY, settings.NETFLIX_SECRET)
+        h.fetch_request_token()
+        url = h.authorize_token()
+
+        raw_input("Visit this URL to authorize your account:\n\n<%s>\n\nthen press Enter: "
+            % url)
+
+        self.access_token = h.fetch_access_token()
+
+        # Confirm that the access token works.
+        h.add_credentials(h.consumer, self.access_token, domain="api.netflix.com")
+        response, content = h.request("http://api.netflix.com/users/current")
+
+        if response.status != 200:
+            raise ValueError('Could not authorize Netflix account')
 
 
 class Hulu(Account):
-    pass
+
+    def configure(self):
+        self.name = raw_input('Enter your Hulu username: ')
+
+        # Confirm that that name has a queue.
+        h = httplib2.Http()
+        url = 'http://www.hulu.com/feed/queue/%s' % self.name
+        response, content = h.request(url, method="HEAD")
+
+        if response.status != 200:
+            raise ValueError('Could not find Hulu account %r' % self.name)
 
 
 services = {
