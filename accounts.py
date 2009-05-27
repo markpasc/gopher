ef9745872bbe8aa51cd59d7e7091edea2ea0585a
Mark Paschal <markpasc@markpasc.org>
2009-05-27 11:40:07 -0700

    Get Netflix instant queue items

--- accounts.py
+++ accounts.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import httplib2
 from oauth.oauth import OAuthConsumer
 from elementtree import ElementTree
@@ -18,12 +20,18 @@ class Account(object):
 
 class Netflix(Account):
 
+    def http(self, access_token=None):
+        h = NetflixHttp()
+        h.consumer = OAuthConsumer(settings.NETFLIX_KEY, settings.NETFLIX_SECRET)
+        if access_token is not None:
+            h.add_credentials(h.consumer, access_token, domain="api.netflix.com")
+        return h
+
     def configure(self):
         if hasattr(self, 'access_token'):
             return
 
-        h = NetflixHttp()
-        h.consumer = OAuthConsumer(settings.NETFLIX_KEY, settings.NETFLIX_SECRET)
+        h = self.http()
         h.fetch_request_token()
         url = h.authorize_token()
 
@@ -33,23 +41,53 @@ class Netflix(Account):
         self.access_token = h.fetch_access_token()
 
         # Confirm that the access token works.
-        h.add_credentials(h.consumer, self.access_token, domain="api.netflix.com")
+        h = self.http(self.access_token)
         response, content = h.request("http://api.netflix.com/users/current")
-
         if response.status != 200:
             raise ValueError('Could not authorize Netflix account')
 
-        # Look for the user name.
+        # Find the real user info.
         culink = ElementTree.fromstring(content)
         userlink = culink.find('./link')
         userhref = userlink.get('href')
 
+        # Look for the user name.
         response, content = h.request(userhref)
+        if response.status != 200:
+            raise ValueError('Could not fetch Netflix account')
         userdoc = ElementTree.fromstring(content)
+        userid = userdoc.find('./user_id').text
         firstname = userdoc.find('./first_name').text
         lastname = userdoc.find('./last_name').text
 
+        self.userid = userid
         self.name = ' '.join((firstname, lastname))
+
+    def itemize_item(self, item):
+        title = item.find('./title').get('regular')
+        link = item.find('./link[@rel="alternate"]').get('href')
+        thumb = item.find('./box_art').get('large')
+
+        timestamp = item.find('./updated').text
+        date = datetime.fromtimestamp(int(timestamp))
+
+        return Item(title=title, location=link, date=date, thumb=thumb)
+
+    def instant_queue(self):
+        h = self.http(self.access_token)
+        response, content = h.request("http://api.netflix.com/users/%s/queues/instant/available"
+            % self.userid)
+        if response.status != 200:
+            raise ValueError('Could not fetch Netflix instant queue %s')
+
+        queue = ElementTree.fromstring(content)
+        items = queue.findall('.//queue_item')
+        if items is None:
+            return
+        return [self.itemize_item(item) for item in items]
+
+    def queue(self):
+        return self.instant_queue()
 
     def __str__(self):
         return "Netflix: %s" % self.name
@@ -71,8 +109,11 @@ class Hulu(Account):
     def itemize_item(self, x):
         title = x.find('title').text
         link = x.find('link').text
-        date = x.find('pubDate').text
         thumb = x.find('{http://search.yahoo.com/mrss/}thumbnail').get('url')
+
+        date = x.find('pubDate').text
+        # TODO: parse date?
+
         return Item(title=title, location=link, date=date, thumb=thumb)
 
     def queue(self):
