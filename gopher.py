e7b5c273af84c39364d92a762ce371539ff856d9
Mark Paschal <markpasc@markpasc.org>
2009-06-03 10:48:18 -0700

    Replace bespoke command system with optfunc

--- gopher.py
+++ gopher.py
@@ -1,52 +1,12 @@
 #!/usr/bin/env python
 
 import logging
-from optparse import OptionParser
 import pickle
 import sys
 
+import optfunc
+
 import accounts
-
-
-class Command(object):
-
-    def configure_logging(self, verbose):
-        levels = {
-            -2: logging.CRITICAL,
-            -1: logging.ERROR,
-             0: logging.WARNING,
-             1: logging.INFO,
-             2: logging.DEBUG,
-        }
-
-        verbose = min(2, max(-2, verbose))
-        level = levels[verbose]
-
-        logging.basicConfig(level=level)
-        logging.info('Logging at level %s', logging.getLevelName(level))
-
-    def add_options(self, parser):
-        pass
-
-    def parse_args(self, argv=None):
-        if argv is None:
-            argv = sys.argv
-
-        parser = OptionParser()
-        self.parser = parser
-        parser.add_option('-v', '--verbose', dest='verbose', action='count',
-                          default=0, help="print more to console")
-        parser.add_option('-q', '--quiet', dest='quiet', action='count',
-                          default=0, help="print less to console")
-        self.add_options(parser)
-        opts, args = parser.parse_args()
-
-        self.configure_logging(opts.verbose - opts.quiet)
-        return opts, args
-
-    def main(self, argv=None):
-        self.parse_args(argv)
-        return 0
 
 
 class Person(object):
@@ -81,52 +41,46 @@ class Person(object):
         logging.info('Saved person %s', self.name)
 
 
-class Gopher(Command):
+@optfunc.arghelp('person', 'person to whom to add the account')
+@optfunc.arghelp('service', 'kind of account to add')
+def add(person, service):
+    """add <person> <service>
 
-    def add_account(self, option, opt, value, parser):
-        parser.values.command = 'add'
-        setattr(parser.values, option.dest, value)
+    Add an account.
 
-    def add_options(self, parser):
-        parser.add_option('-p', '--person', dest='name',
-            help="person to act as")
-        parser.add_option('--add', action='callback', dest='account',
-            type='string', callback=self.add_account,
-            help="add an account", metavar='SERVICE')
-        parser.add_option('--accounts', action='store_const', dest='command',
-            const='accounts', help="list all of a person's accounts")
-        parser.add_option('--queue', action='store_const', dest='command',
-            const='queue', help="compile and print a person's queue")
+    """
+    p = Person.get(person)
+    p.add_account(service)
+    p.save()
 
-    def main(self, argv=None):
-        opt, arg = self.parse_args(argv)
 
-        if opt.name is None:
-            self.parser.error("Person parameter is required")
+@optfunc.arghelp('person', 'person whose accounts to list')
+def accounts(person):
+    """accounts <person>
 
-        p = Person.get(opt.name)
+    List all of a person's accounts.
 
-        if opt.command is None:
-            print "No command was specified"
-            return 1
+    """
+    p = Person.get(person)
+    print "## Accounts ##"
+    for x in p.accounts.values():
+        print x
 
-        if opt.command == 'add':
-            p.add_account(opt.account)
-            p.save()
-        elif opt.command == 'accounts':
-            print "## Accounts ##"
-            for x in p.accounts.values():
-                print x
-        elif opt.command == 'queue':
-            queue = list()
-            for servicename, account in p.accounts.iteritems():
-                queue.extend(account.queue())
-            print "## Queue ##"
-            for x in sorted(queue, key=lambda x: x.date):
-                print x
 
-        return 0
+@optfunc.arghelp('person', 'person whose queue to show')
+def queue(person):
+    """queue <person>
+
+    Compile and print a person's queue.
+
+    """
+    queue = list()
+    for servicename, account in p.accounts.iteritems():
+        queue.extend(account.queue())
+    print "## Queue ##"
+    for x in sorted(queue, key=lambda x: x.date):
+        print x
 
 
 if __name__ == '__main__':
-    sys.exit(Gopher().main())
+    optfunc.run((add, accounts, queue))
